a5eba08d82e6bb6178413b73eda0c5c7469432e8
Izzat-Hasan <izzathasan03@gmail.com>
2021-11-05 14:52:23 -0500

    intial commit

---
+++ gghivsitpe.js
@@ -0,0 +1,13 @@
+{
+    "firstName": "Tom ",
+    "lastName": "Holland",
+    "preferredName": "SpiderMan",
+    "email": "Spider-Man@lewisu.edu",
+    "phoneNumber": "800-Spider-Man",
+    "city": "Queens",
+    "state": "New York",
+    "zip": "11428",
+    "lat": "40.7282",
+    "lng": "-73.7949",
+    "favoriteHobby": "saving people"
+}
